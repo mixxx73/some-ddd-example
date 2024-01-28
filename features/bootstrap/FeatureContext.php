4c19921af55e06c2989f02cf889da8f5d57cd837
mixxx73 <m.macierzynski@gmail.com>
2024-01-28 11:53:58 +0100

    wip

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,21 +1,29 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Ubirak\RestApiBehatExtension\Rest\RestApiBrowser;
 use Ubirak\RestApiBehatExtension\RestApiContext;
+use Doctrine\DBAL\Connection;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+//    public function __construct(private Symfony\Bridge\Doctrine\ $connection)
+//    {
+//    }
+
 
     /** @BeforeScenario */
-    public function before(BeforeScenarioScope $scope)
-    {
-        $this->dm->getConnection()->dropDatabase($this->dbName);
-    }
+//    public function before(BeforeScenarioScope $scope)
+//    {
+//        $platform = $this->connection->getDatabasePlatform();
+//
+//        $this->connection->executeQuery($platform->getTruncateTableSQL('user', true));
+//    }
 
 }
